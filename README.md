291f32943f3b668369d324cd9afacd1a5dcbdd1f
René Filip <renefilip@mail.com>
2016-06-09 20:22:45 +0200

    create all nec. files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Travis deploy script provider
+
+`.travis.yml` file for using the script provider inside the `deploy` section.
+
+## How to:
+
+TODO
---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "To kill a fly with a sledge-hammer. [1]\n
+Theorem. n√2 is irrational for every n ≥ 3.
+Proof. Suppose n√2 is rational, say n√2 = p / q.
+Then p^n = q^n + q^n, contradicting Fermat's Last Theorem.\n
+Unfortunately FLT is not strong enough to prove √2 irrational.\n
+[1] W.H. Schultz, An observation, American Mathematical Monthly, Vol. 110, Nr. 5, May 2003. (submitted by R. Ehrenborg)"
